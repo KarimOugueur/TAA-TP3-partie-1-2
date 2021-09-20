d116615508e791823d0e53fa70655ef3a7a9d66f
KarimOugueur <ougueur.k@gmail.com>
2021-09-20 23:59:23 +0200

    tp3 partie finished

--- src/main/java/tp3/partie1/securityApplication/SecureApp.java
+++ src/main/java/tp3/partie1/securityApplication/SecureApp.java
@@ -1,18 +1,19 @@
-package tp3.partie1.traceBeans;
+package tp3.partie1.securityApplication;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.*;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
 
 @Aspect
 @Component
 
 public class SecureApp {
     
-	//Utilisation de l'annotation @Around autour de la méthode transfert
-	
-	@Around("execution(public * sample.simple.bank..transfert(..))")
 	public void log(ProceedingJoinPoint joinPoint) throws Throwable{
+
 		System.err.println("****LoggingAspect- Before bank method call");  
 		joinPoint.proceed();
 		System.err.println("****LoggingAspect- After bank method call");  
 	}
+}
